cec571c61d7837599604a1b000e582ae8aa90fbe
ahpicl64 <ubuntu@ip-172-31-2-144.ap-northeast-2.compute.internal>
2025-04-29 01:50:35 +0900

    feat : coalesce function complete

--- mm.c
+++ mm.c
@@ -62,7 +62,38 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+static void *coalesce(void *bp){
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(FTRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
 
+    if (prev_alloc && next_alloc) {             /* Case 1 */
+        return bp;
+    }
+
+    else if (prev_alloc && !next_alloc) {       /* Case 2 */
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    }
+
+    else if (!prev_alloc && next_alloc) {       /* Case 3 */
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+
+    else {                                      /* Case 4 */
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) +
+            GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)),PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)),PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    return bp;
+    
+}
 
 static void *extend_heap(size_t words){
     char *bp;
